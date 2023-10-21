2dc959d9133db4b0a2497d252223842c7c4f22ac
valli0x <youarethesame1821@gmail.com>
2023-10-21 16:02:25 +0300

    test for schnorr keygen

--- stages/keygen/keygen_test.go
+++ stages/keygen/keygen_test.go
@@ -8,16 +8,21 @@ import (
 	"sync"
 	"testing"
 
+	"github.com/btcsuite/btcd/btcec/v2/schnorr"
+	"github.com/btcsuite/btcd/btcutil"
+	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/hashicorp/go-hclog"
 	"github.com/taurusgroup/multi-party-sig/pkg/party"
 	"github.com/taurusgroup/multi-party-sig/pkg/pool"
 	"github.com/taurusgroup/multi-party-sig/protocols/cmp"
+	"github.com/taurusgroup/multi-party-sig/protocols/frost"
 	"github.com/valli0x/signature-escrow/network/redis"
-	"github.com/valli0x/signature-escrow/stages/keygen/eth"
+	"github.com/valli0x/signature-escrow/stages/keygen/btcfrost"
+	"github.com/valli0x/signature-escrow/stages/keygen/ethcmp"
 )
 
-func TestKeygenETH(t *testing.T) {
+func TestKeygenCMP(t *testing.T) {
 	logger := hclog.NewInterceptLogger(&hclog.LoggerOptions{
 		Output:     os.Stdout,
 		Level:      hclog.DefaultLevel,
@@ -44,7 +49,7 @@ func TestKeygenETH(t *testing.T) {
 		defer pl1.TearDown()
 
 		fmt.Println("start a keygen")
-		configA, err = eth.CMPKeygen("a", party.IDSlice{"a", "b"}, 1, net1, pl1)
+		configA, err = ethcmp.CMPKeygen("a", party.IDSlice{"a", "b"}, 1, net1, pl1)
 		if err != nil {
 			fmt.Println("error a", err)
 			return
@@ -59,7 +64,7 @@ func TestKeygenETH(t *testing.T) {
 		defer pl2.TearDown()
 
 		fmt.Println("start b keygen")
-		configB, err = eth.CMPKeygen("b", party.IDSlice{"a", "b"}, 1, net2, pl2)
+		configB, err = ethcmp.CMPKeygen("b", party.IDSlice{"a", "b"}, 1, net2, pl2)
 		if err != nil {
 			fmt.Println("error a", err)
 			return
@@ -70,17 +75,17 @@ func TestKeygenETH(t *testing.T) {
 
 	wg.Wait()
 
-	if err := PrintAddressPubKey("a", configA, logger.NamedIntercept("a")); err != nil {
-		t.Fatal("print address pub key a", err)
+	if err := PrintAddressPubKeyECDSA("a", configA, logger.NamedIntercept("a")); err != nil {
+		t.Fatal("print address pub key a:", err)
 	}
 
-	if err := PrintAddressPubKey("b", configB, logger.NamedIntercept("b")); err != nil {
-		t.Fatal("print address pub key b", err)
+	if err := PrintAddressPubKeyECDSA("b", configB, logger.NamedIntercept("b")); err != nil {
+		t.Fatal("print address pub key b:", err)
 	}
 }
 
-func PrintAddressPubKey(name string, c *cmp.Config, logger hclog.InterceptLogger) error {
-	pubkeyECDSA, err := GetPubKeyFromConfig(c)
+func PrintAddressPubKeyECDSA(name string, c *cmp.Config, logger hclog.InterceptLogger) error {
+	pubkeyECDSA, err := GetPubKeyFromConfigECDSA(c)
 	if err != nil {
 		return err
 	}
@@ -94,7 +99,7 @@ func PrintAddressPubKey(name string, c *cmp.Config, logger hclog.InterceptLogger
 	return nil
 }
 
-func GetPubKeyFromConfig(keygenConfig *cmp.Config) (*crypto_ecdsa.PublicKey, error) {
+func GetPubKeyFromConfigECDSA(keygenConfig *cmp.Config) (*crypto_ecdsa.PublicKey, error) {
 	// get from address
 	publicKey, _ := keygenConfig.PublicPoint().MarshalBinary()
 	publicKeyECDSA, err := crypto.DecompressPubkey(publicKey)
@@ -103,4 +108,86 @@ func GetPubKeyFromConfig(keygenConfig *cmp.Config) (*crypto_ecdsa.PublicKey, err
 	}
 
 	return publicKeyECDSA, nil
+}
+
+func TestKeygenFROST(t *testing.T) {
+	logger := hclog.NewInterceptLogger(&hclog.LoggerOptions{
+		Output:     os.Stdout,
+		Level:      hclog.DefaultLevel,
+		JSONFormat: false,
+	})
+
+	net1, err := redis.NewRedisNet("localhost:6379", "a", "b", logger.Named("a"))
+	if err != nil {
+		t.Fatal("net a error", err)
+		return
+	}
+
+	net2, err := redis.NewRedisNet("localhost:6379", "b", "a", logger.Named("b"))
+	if err != nil {
+		t.Fatal("net a error", err)
+	}
+
+	var configA, configB *frost.TaprootConfig
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		fmt.Println("start a keygen")
+		configA, err = btcfrost.FrostKeygenTaproot("a", party.IDSlice{"a", "b"}, 1, net1)
+		if err != nil {
+			fmt.Println("error a", err)
+			return
+		}
+		fmt.Println("end a keygen")
+		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func() {
+		fmt.Println("start b keygen")
+		configB, err = btcfrost.FrostKeygenTaproot("b", party.IDSlice{"a", "b"}, 1, net2)
+		if err != nil {
+			fmt.Println("error a", err)
+			return
+		}
+		fmt.Println("end b keygen")
+		wg.Done()
+	}()
+
+	wg.Wait()
+
+	if err := PrintAddressPubKeyTaproot("a", configA, logger.NamedIntercept("a")); err != nil {
+		t.Fatal("print address pub key a:", err)
+	}
+
+	if err := PrintAddressPubKeyTaproot("b", configB, logger.NamedIntercept("b")); err != nil {
+		t.Fatal("print address pub key b:", err)
+	}
+}
+
+func PrintAddressPubKeyTaproot(name string, c *frost.TaprootConfig, logger hclog.InterceptLogger) error {
+	pubkeyECDSA, err := GetPubKeyFromConfigTaproot(c)
+	if err != nil {
+		return err
+	}
+
+	pub := crypto.FromECDSAPub(pubkeyECDSA)
+	address, err := btcutil.NewAddressPubKeyHash(btcutil.Hash160(pub), &chaincfg.MainNetParams)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("config", name)
+	logger.Info("BTC info", "address", address)
+	logger.Info("BTC info", "public key", hex.EncodeToString(pub))
+	return nil
+}
+
+func GetPubKeyFromConfigTaproot(keygenConfig *frost.TaprootConfig) (*crypto_ecdsa.PublicKey, error) {
+	publicKeyECDSA, err := schnorr.ParsePubKey(keygenConfig.PublicKey)
+	if err != nil {
+		return nil, err
+	}
+	return publicKeyECDSA.ToECDSA(), nil
 }
